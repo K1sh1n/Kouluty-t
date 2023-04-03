794aeb6e27899358a50fea81a5802485ac5ff822
K1sh1n <116551584+K1sh1n@users.noreply.github.com>
2023-04-03 10:29:28 +0300

    nuoli 3.3

--- Nuolia kaupan/Nuolia kaupan/Program.cs
+++ Nuolia kaupan/Nuolia kaupan/Program.cs
@@ -8,41 +8,38 @@ string karki = ReadConsole("Minkälainen kärki?(puu, teräs vai timantti)");
 string pera = ReadConsole("Minkälainen perä?(lehti, kanansulka vai kotkansulka");
 int pituus = int.Parse(ReadConsole("Nuolen pituus (60cm - 100cm)"));
 
-Nuoli nuoli = new Nuoli()
-{
-    karki = Enum.Parse<Karki>(karki),
-    pera = Enum.Parse<Pera>(pera),
-    pituus = pituus
-};
+Nuoli nuoli = new Nuoli(Enum.Parse<Karki>(karki), Enum.Parse<Pera>(pera), pituus);
+
 int hinta = nuoli.PalautaHinta();
 Console.WriteLine("Tämän nuolen hinta on " + hinta + " kultaa");
 class Nuoli {
-    private Karki karki;
-    private Pera pera;
-    private int pituus;
-
-    public Karki GetKarki()
+    public Karki Karki
     {
-        return karki;
+        get;
     }
-    public Pera GetPera()
+    public Pera Pera
     {
-        return pera;
+        get;
     }
-    public int GetPituus()
+    public int Pituus {
+        get;}
+    public Nuoli(Karki karki, Pera pera, int pituus)
     {
-        return pituus;
+        Karki = karki;
+        Pera = pera;
+        Pituus = pituus;
     }
     public int PalautaHinta()
     {
-        int karkiHinta = (int)karki;
-        int peraHinta = (int)pera;
-        int varsiHinta = (int)(0.05 * pituus);
+        int karkiHinta = (int)Karki;
+        int peraHinta = (int)Pera;
+        int varsiHinta = (int)(0.05 * Pituus);
         return karkiHinta + peraHinta + varsiHinta;
     }
 }
 enum Karki { puu = 3, teräs = 5, timantti = 50 };
 enum Pera { lehti = 0, kanansulka = 1, kotkansulka = 5 };
+
 
 
 
