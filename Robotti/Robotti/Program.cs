0be328b0d40bd61dbca29eb8b46eba20ef6f93da
Leon Bui <s2100950@edu.bc.fi>
2024-01-25 12:04:59 +0200

    tehtävä5.2

--- Robotti/Robotti/Program.cs
+++ Robotti/Robotti/Program.cs
@@ -36,41 +36,42 @@ public class Robotti
     public int X { get; set; }
     public int Y { get; set; }
     public bool OnKäynnissä { get; set; }
-    public RobottiKäsky?[] Käskyt { get; } = new RobottiKäsky?[3];
+    public IRobottiKäsky?[] Käskyt { get; } = new IRobottiKäsky?[3];
 
     public void Suorita()
     {
-        foreach (RobottiKäsky? käsky in Käskyt)
+        foreach (IRobottiKäsky? käsky in Käskyt)
         {
             käsky?.Suorita(this);
             Console.WriteLine($"[{X} {Y} {OnKäynnissä}]");
         }
     }
 }
-public abstract class RobottiKäsky
+
+public interface IRobottiKäsky
 {
     public abstract void Suorita(Robotti robotti);
 }
 
-public class Käynnistä : RobottiKäsky
+public class Käynnistä : IRobottiKäsky
 {
-    public override void Suorita(Robotti robotti)
+    public void Suorita(Robotti robotti)
     {
         robotti.OnKäynnissä = true;
     }
 }
 
-public class Sammuta : RobottiKäsky
+public class Sammuta : IRobottiKäsky
 {
-    public override void Suorita(Robotti robotti)
+    public void Suorita(Robotti robotti)
     {
         robotti.OnKäynnissä = false;
     }
 }
 
-public class YlösKäsky : RobottiKäsky
+public class YlösKäsky : IRobottiKäsky
 {
-    public override void Suorita(Robotti robotti)
+    public void Suorita(Robotti robotti)
     {
         if (robotti.OnKäynnissä)
         {
@@ -79,9 +80,9 @@ public class YlösKäsky : RobottiKäsky
     }
 }
 
-public class AlasKäsky : RobottiKäsky
+public class AlasKäsky : IRobottiKäsky
 {
-    public override void Suorita(Robotti robotti)
+    public void Suorita(Robotti robotti)
     {
         if (robotti.OnKäynnissä)
         {
@@ -90,9 +91,9 @@ public class AlasKäsky : RobottiKäsky
     }
 }
 
-public class VasenKäsky : RobottiKäsky
+public class VasenKäsky : IRobottiKäsky
 {
-    public override void Suorita(Robotti robotti)
+    public void Suorita(Robotti robotti)
     {
         if (robotti.OnKäynnissä)
         {
@@ -101,9 +102,9 @@ public class VasenKäsky : RobottiKäsky
     }
 }
 
-public class OikeaKäsky : RobottiKäsky
+public class OikeaKäsky : IRobottiKäsky
 {
-    public override void Suorita(Robotti robotti)
+    public void Suorita(Robotti robotti)
     {
         if (robotti.OnKäynnissä)
         {
